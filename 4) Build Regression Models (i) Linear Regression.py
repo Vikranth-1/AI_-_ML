d8d68d050f816b0926d79b2e85f795277d66a49a
Vikranth V <vvikranth08@gmail.com>
2026-05-04 12:04:01 +0530

    Update 4) Build Regression Models (i) Linear Regression.py

--- 4) Build Regression Models (i) Linear Regression.py
+++ 4) Build Regression Models (i) Linear Regression.py
@@ -1,17 +1,20 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("ai_ml_lab_data.csv")
 
 X = df[['experience']]
 y = df['salary']
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
 model = LinearRegression()
-model.fit(X,y)
+model.fit(X_train, y_train)
 
-y_pred = model.predict(X)
+y_pred = model.predict(X_test)
 
-plt.scatter(X,y)
-plt.plot(X,y_pred)
+plt.scatter(X, y)
+plt.plot(X, model.predict(X))
 plt.show()
